abe9a15a69d1d166ecbe8f6b3f31ce5ca1e05272
ShriramG24 <shrirambhavin@gmail.com>
2024-09-18 12:44:53 -0400

    Apply black and isort

--- src/client.py
+++ src/client.py
@@ -1,4 +1,6 @@
-import os, shutil
+import os
+import shutil
+
 from flask_ml.flask_ml_client import MLClient
 from flask_ml.flask_ml_server.constants import DataTypes
 
@@ -6,7 +8,7 @@ INPUT_DIR = "input"
 OUTPUT_DIR = "output"
 SR_MODEL_URL = "http://localhost:5000/super-resolution"
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if os.path.exists(OUTPUT_DIR):
         shutil.rmtree(OUTPUT_DIR)
     os.makedirs(OUTPUT_DIR)
@@ -15,9 +17,9 @@ if __name__ == '__main__':
     data_type = DataTypes.IMAGE
     inputs = []
     for filename in os.listdir(INPUT_DIR):
-        if filename.endswith(('.png', '.jpg', '.jpeg')):
-            name = filename.split('.')[0].split('/')[-1]
-            inputs.append({ 'file_path': f'{INPUT_DIR}/{filename}' })
-    
-    response = client.request(inputs, data_type, { 'output_dir': OUTPUT_DIR })
+        if filename.endswith((".png", ".jpg", ".jpeg")):
+            name = filename.split(".")[0].split("/")[-1]
+            inputs.append({"file_path": f"{INPUT_DIR}/{filename}"})
+
+    response = client.request(inputs, data_type, {"output_dir": OUTPUT_DIR})
     print(response)
--- src/server.py
+++ src/server.py
@@ -1,42 +1,47 @@
-import logging, os
+import logging
+import os
+
 import numpy as np
 from PIL import Image
 
 logging.disable(logging.WARNING)
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
-from ISR.models import RRDN
 from flask_ml.flask_ml_server import MLServer
 from flask_ml.flask_ml_server.constants import DataTypes
-from flask_ml.flask_ml_server.models import ResponseModel, ImageResult
+from flask_ml.flask_ml_server.models import ImageResult, ResponseModel
+from ISR.models import RRDN
+
 
 class SuperResolution:
     def predict(self, data: list[dict], output_dir: str) -> list[dict]:
-        rrdn = RRDN(weights='gans')
+        rrdn = RRDN(weights="gans")
         results = []
         for img in data:
             lr_img = np.array(Image.open(img.file_path))
             sr_img = Image.fromarray(rrdn.predict(lr_img))
 
-            name = img.file_path.split('/')[-1].split('.')[0]
-            sr_img.save(f'{output_dir}/{name}-hr.jpg')
-            results.append({
-                'file_path': img.file_path,
-                'result': f'{output_dir}/{name}-hr.jpg'
-            })
+            name = img.file_path.split("/")[-1].split(".")[0]
+            sr_img.save(f"{output_dir}/{name}-hr.jpg")
+            results.append(
+                {"file_path": img.file_path, "result": f"{output_dir}/{name}-hr.jpg"}
+            )
         return results
+
 
 model = SuperResolution()
 server = MLServer(__name__)
 
+
 @server.route("/super-resolution", DataTypes.IMAGE)
 def super_resolution(inputs: list, parameters: dict):
     results = [
-        ImageResult(file_path=res['file_path'], result=res['result'])
-        for res in model.predict(inputs, parameters['output_dir'])
+        ImageResult(file_path=res["file_path"], result=res["result"])
+        for res in model.predict(inputs, parameters["output_dir"])
     ]
 
     return ResponseModel(results=results).get_response()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     server.run()
