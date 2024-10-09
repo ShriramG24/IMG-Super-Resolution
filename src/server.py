122044847341d54d33ce7f1b802789b35d7f4405
Shriram Giridhara <shrirambhavin@gmail.com>
2024-10-09 13:39:12 -0400

    Make changes to work with RescueBox UI

--- src/server.py
+++ src/server.py
@@ -1,22 +1,53 @@
+import os
+
 from flask_ml.flask_ml_server import MLServer
 from flask_ml.flask_ml_server.constants import DataTypes
 from flask_ml.flask_ml_server.models import ImageResult, ResponseModel
+from flask import request
 
 from super_resolution import SuperResolution
 
 model = SuperResolution()
 server = MLServer(__name__)
 
+# @server.route("/run-inference", DataTypes.Image)
+# def run_inference():
+#     body = request.get_json()
+#     inputs = []
+#     for filename in os.listdir(body['inputDir']):
+#         if filename.endswith((".png", ".jpg", ".jpeg")):
+#             inputs.append({"file_path": f"{body['inputDir']}/{filename}"})
 
-@server.route("/super-resolution", DataTypes.IMAGE)
-def super_resolution(inputs: list, parameters: dict):
+#     results = [
+#         ImageResult(file_path=res["file_path"], result=res["result"])
+#         for res in model.predict(inputs, body['outputDir'])
+#     ]
+
+#     return ResponseModel(results=results).get_response()
+
+@server.app.post("/run-inference")
+def run_inference():
+    body = request.get_json()
+    print(body)
+    inputs = []
+    for filename in os.listdir(body['inputDir']):
+        if filename.endswith((".png", ".jpg", ".jpeg")):
+            inputs.append({"file_path": f"{body['inputDir']}/{filename}"})
+
     results = [
         ImageResult(file_path=res["file_path"], result=res["result"])
-        for res in model.predict(inputs, parameters["output_dir"])
+        for res in model.predict(inputs, {
+            "outputDir": body['outputDir'],
+            "weights": body['parameters'][0]['weights'],
+            "scale": float(body['parameters'][0]['scale'])
+        })
     ]
 
     return ResponseModel(results=results).get_response()
 
+@server.app.get("/health")
+def health():
+    return {"status": 200}
 
 if __name__ == "__main__":
     server.run()
--- src/super_resolution.py
+++ src/super_resolution.py
@@ -7,20 +7,25 @@ from PIL import Image
 logging.disable(logging.WARNING)
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
-from ISR.models import RRDN
+from ISR.models import RRDN, RDN
 
 
 class SuperResolution:
-    def predict(self, data: list[dict], output_dir: str) -> list[dict]:
-        rrdn = RRDN(weights="gans")
+    def predict(self, data: list[dict], params: dict) -> list[dict]:
+        weights = params.get("weights", "gans")
+        output_dir = params.get("outputDir")
+        scale = float(params.get("scale", 4.0))
+
+        model = RRDN(weights=weights) if weights == 'gans' else RDN(weights=weights)
         results = []
         for img in data:
-            lr_img = np.array(Image.open(img.file_path))
-            sr_img = Image.fromarray(rrdn.predict(lr_img))
+            lr_img = np.array(Image.open(img['file_path']))
+            new_w, new_h = int(lr_img.shape[1] * scale), int(lr_img.shape[0] * scale)
+            sr_img = Image.fromarray(model.predict(lr_img)).resize((new_w, new_h))
 
-            name = img.file_path.split("/")[-1].split(".")[0]
-            sr_img.save(f"{output_dir}/{name}-hr.jpg")
+            name = img['file_path'].split("/")[-1]
+            sr_img.save(f"{output_dir}/{name}")
             results.append(
-                {"file_path": img.file_path, "result": f"{output_dir}/{name}-hr.jpg"}
+                {"file_path": img['file_path'], "result": f"{output_dir}/{name}"}
             )
         return results
