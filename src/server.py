b7b78a74e84a671c7a0e7b5d393dabd3d70cf120
Atharva Kale <atharvakale343@gmail.com>
2024-10-24 07:44:49 -0400

    upgrade to flask 0.2.2

--- src/server.py
+++ src/server.py
@@ -1,22 +1,61 @@
+from typing import TypedDict
 from flask_ml.flask_ml_server import MLServer
-from flask_ml.flask_ml_server.constants import DataTypes
-from flask_ml.flask_ml_server.models import ImageResult, ResponseModel
+from flask_ml.flask_ml_server.models import (
+    FileResponse,
+    ResponseBody,
+    BatchFileInput,
+    FileType,
+    DirectoryInput,
+    BatchFileResponse,
+    TaskSchema,
+    InputSchema,
+    InputType,
+)
 
-from super_resolution import SuperResolution
+from .super_resolution import SuperResolution
 
 model = SuperResolution()
 server = MLServer(__name__)
 
 
-@server.route("/super-resolution", DataTypes.IMAGE)
-def super_resolution(inputs: list, parameters: dict):
+def create_task_schema() -> TaskSchema:
+    return TaskSchema(
+        inputs=[
+            InputSchema(
+                input_type=InputType.BATCHFILE,
+                key="input_images",
+                subtitle="Images to be upscaled",
+                label="Input Images",
+            ),
+            InputSchema(
+                input_type=InputType.DIRECTORY,
+                key="output_directory",
+                label="Output Directory",
+                subtitle="Directory to save the upscaled images",
+            ),
+        ],
+        parameters=[],
+    )
+
+
+class Inputs(TypedDict):
+    input_images: BatchFileInput
+    output_directory: DirectoryInput
+
+
+class Parameters(TypedDict):
+    pass
+
+
+@server.route("/super-resolution", task_schema_func=create_task_schema, short_title="Super Resolution")
+def super_resolution(inputs: Inputs, parameters: Parameters) -> ResponseBody:
     results = [
-        ImageResult(file_path=res["file_path"], result=res["result"])
-        for res in model.predict(inputs, parameters["output_dir"])
+        FileResponse(title=res["file_path"], path=res["result"], file_type=FileType.IMG)
+        for res in model.predict(inputs["input_images"].files, inputs["output_directory"].path)
     ]
 
-    return ResponseModel(results=results).get_response()
+    return ResponseBody(root=BatchFileResponse(files=results))
 
 
 if __name__ == "__main__":
-    server.run()
+    server.run(host="0.0.0.0", port=5001)
--- src/super_resolution.py
+++ src/super_resolution.py
@@ -4,23 +4,23 @@ import os
 import numpy as np
 from PIL import Image
 
-logging.disable(logging.WARNING)
+# logging.disable(logging.WARNING)
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
-from ISR.models import RRDN
-
+from flask_ml.flask_ml_server.models import FileInput
 
 class SuperResolution:
-    def predict(self, data: list[dict], output_dir: str) -> list[dict]:
+    def predict(self, data: list[FileInput], output_dir: str) -> list[dict]:
+        from ISR.models import RRDN
         rrdn = RRDN(weights="gans")
         results = []
         for img in data:
-            lr_img = np.array(Image.open(img.file_path))
+            lr_img = np.array(Image.open(img.path))
             sr_img = Image.fromarray(rrdn.predict(lr_img))
 
-            name = img.file_path.split("/")[-1].split(".")[0]
+            name = img.path.split("/")[-1].split(".")[0]
             sr_img.save(f"{output_dir}/{name}-hr.jpg")
             results.append(
-                {"file_path": img.file_path, "result": f"{output_dir}/{name}-hr.jpg"}
+                {"file_path": img.path, "result": f"{output_dir}/{name}-hr.jpg"}
             )
         return results
