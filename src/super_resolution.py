e005bd1cb51f7a790b3d0cee812d370e3ebee3f4
ShriramG24 <shrirambhavin@gmail.com>
2024-10-24 13:05:45 -0400

    Create output_dir if does'nt exist

--- src/super_resolution.py
+++ src/super_resolution.py
@@ -1,10 +1,8 @@
-import logging
 import os
 
 import numpy as np
 from PIL import Image
 
-# logging.disable(logging.WARNING)
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
 from flask_ml.flask_ml_server.models import FileInput
@@ -13,6 +11,9 @@ class SuperResolution:
     def predict(self, data: list[FileInput], output_dir: str, weights='gans', scale=4.0) -> list[dict]:
         from ISR.models import RRDN, RDN
         model = RRDN(weights=weights) if weights == 'gans' else RDN(weights=weights)
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
         results = []
         for img in data:
             lr_img = np.array(Image.open(img.path))
